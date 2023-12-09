c981eabd6ebc2dbb76016e09474eb0f557acbe6b
tiankun <847415859@qq.com>
2023-12-09 14:14:16 +0800

    设计模式案例项目init

---
+++ general-design/src/main/java/com/qiankun/App.java
@@ -0,0 +1,13 @@
+package com.qiankun;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
